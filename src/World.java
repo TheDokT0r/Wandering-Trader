eece6f352514249386f9eb518ed1d34d4d03aab6
TheDokTor <orkv1234@gmail.com>
2022-08-10 21:06:19 +0300

    Worlds Loader

--- src/World.java
+++ src/World.java
@@ -1,13 +1,19 @@
 public class World {
     private int ID;
+    public final String WorldName;
     public Player Player;
     public City[] Cities;
+    public int StartingCityID;
     public Shop[] Shops;
+    public Item[] Items; //All available items that can exist in the game
 
-    public World(int ID, Player player, City[] cities, Shop[] shops) {
+    public World(String worldName, int ID, Player player, City[] cities, Shop[] shops, Item[] items, int startingCityID) {
         this.ID = ID;
+        WorldName = worldName;
         Player = player;
         Cities = cities;
         Shops = shops;
+        Items = items;
+        StartingCityID = startingCityID;
     }
 }
--- src/WorldsMannager.kt
+++ src/WorldsMannager.kt
@@ -1,4 +1,10 @@
+
+import org.json.simple.JSONArray
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
 import java.io.File
+import java.io.FileReader
+
 
 class WorldsMannager() {
     private val worldsFolderPath: String = "./Worlds/"
@@ -12,4 +18,31 @@ class WorldsMannager() {
 
         return worldsArr.toTypedArray()
     }
+
+    fun getWorldData(mainDirPath: String) {
+        val info: Map<String, String> = getInfoJson(mainDirPath)
+    }
+
+    fun getInfoJson(path: String): Map<String, String> {
+        val obj = JSONParser().parse(FileReader("$path/info.json"))
+        val jo: JSONObject = obj as JSONObject
+
+        val worldName: String = jo["world_name"] as String
+        val startingCity: String = jo["starting_city"] as String
+        val currency: String = jo["currency_symbol"] as String
+        val bal: String = jo["starting_bal"] as String
+
+        return mapOf("worldName" to worldName, "startingCity" to startingCity, "currency" to currency, "bal" to bal)
+    }
+
+    fun getItems(path: String): Array<Item> {
+        val obj = JSONParser().parse(FileReader("$path/data/items.json"))
+        val jo: JSONObject = obj as JSONObject
+        val ja: JSONArray = jo["data"] as JSONArray
+
+        var items = arrayListOf<Item>()
+
+        for (item in ja) {
+
+        }
 }
